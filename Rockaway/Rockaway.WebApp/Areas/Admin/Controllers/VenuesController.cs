352846b5d5619a32a363c4b0001342a9b8866903
Dylan Beattie <dylan@dylanbeattie.net>
2024-08-27 10:13:12 +0200

    Replace country code with culture name for Venues edit in admin area

--- Rockaway/Rockaway.WebApp/Areas/Admin/Controllers/VenuesController.cs
+++ Rockaway/Rockaway.WebApp/Areas/Admin/Controllers/VenuesController.cs
@@ -46,7 +46,7 @@ public class VenuesController(RockawayDbContext context) : Controller {
 	// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
 	[HttpPost]
 	[ValidateAntiForgeryToken]
-	public async Task<IActionResult> Edit(Guid id, [Bind("Id,Name,Slug,Address,City,CountryCode,PostalCode,Telephone,WebsiteUrl")] Venue venue) {
+	public async Task<IActionResult> Edit(Guid id, [Bind("Id,Name,Slug,Address,City,CultureName,PostalCode,Telephone,WebsiteUrl")] Venue venue) {
 		if (id != venue.Id) return NotFound();
 		if (!ModelState.IsValid) return View(venue);
 		try {
